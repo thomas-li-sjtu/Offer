657cdd1c49c4cf18124ecd1f9e982edb7b3b0b26
thomas-li-sjtu <980226547@qq.com>
2020-12-17 12:39:30 +0800

    Easy

--- JZ45_Is continuous/solution.py
+++ JZ45_Is continuous/solution.py
@@ -1,10 +1,10 @@
 # -*- coding:utf-8 -*-
 class Solution:
-    def IsContinuous(self, numbers:list):
+    def IsContinuous(self, numbers: list):
         # write code here
         kings = numbers.count(0)
         numbers = [i for i in sorted(numbers) if i != 0]
-        sub = [numbers[i+1]-numbers[i]-1 for i in range(len(numbers)-1)]
+        sub = [numbers[i + 1] - numbers[i] - 1 for i in range(len(numbers) - 1)]
         if len(numbers) == 0:  # special case
             return 0
         for i in sub:  # special case
