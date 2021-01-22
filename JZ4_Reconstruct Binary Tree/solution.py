c33f5be78c72bb5e05cfd4bed2e763a13330916f
thomas-li-sjtu <980226547@qq.com>
2021-01-22 12:47:21 +0800

    递归重建二叉树

--- JZ4_Reconstruct Binary Tree/solution.py
+++ JZ4_Reconstruct Binary Tree/solution.py
@@ -4,6 +4,8 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
+
+
 class Solution:
     # 返回构造的TreeNode根节点
     def reConstructBinaryTree(self, pre, tin):
