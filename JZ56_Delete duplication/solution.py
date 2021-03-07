b44fdfeecbedaa0ccbbf3f38d7627c5b32ffe725
thomas-li-sjtu <980226547@qq.com>
2021-03-07 18:38:18 +0800

    顺序调换

--- JZ56_Delete duplication/solution.py
+++ JZ56_Delete duplication/solution.py
@@ -1,30 +1,28 @@
 # -*- coding:utf-8 -*-
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
 class Solution:
-    # 返回对应char
-    def __init__(self):
-        self.data = []
-        self.once = []
-        self.dict = {}
-
-    def FirstAppearingOnce(self):
+    def deleteDuplication(self, pHead: ListNode):
         # write code here
-        return self.once[0] if len(self.once) != 0 else '#'
+        if not pHead:
+            return None
+        data = []
+        while pHead.next:
+            data.append(pHead.val)
+            pHead = pHead.next
+        data.append(pHead.val)
+        data = [i for i in data if data.count(i) == 1]
+        if not data:  # special case：[1,1,1,1,1]
+            return None
 
-    def Insert(self, char):
-        # write code here
-        if self.dict.get(char):
-            self.dict[char] = self.dict[char] + 1
-            if self.dict[char] == 2:
-                self.once.remove(char)
-        else:
-            self.dict[char] = 1
-            self.once.append(char)
-        return self.FirstAppearingOnce()
-
-# s = Solution()
-# print(s.Insert('g'))
-# print(s.Insert('o'))
-# print(s.Insert('o'))
-# print(s.Insert('g'))
-# print(s.Insert('l'))
-# print(s.Insert('e'))
+        tmp = ListNode(data[0])
+        pHead = tmp
+        for i in range(1, len(data)):
+            tmpNode = ListNode(data[i])
+            pHead.next = tmpNode
+            pHead = pHead.next
+        return tmp
