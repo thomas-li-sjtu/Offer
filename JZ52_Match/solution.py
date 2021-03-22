801c3384bce595794db8734ef7a508c1d2b532fe
thomas-li-sjtu <980226547@qq.com>
2021-03-22 10:12:26 +0800

    状态转移矩阵

--- JZ52_Match/solution.py
+++ JZ52_Match/solution.py
@@ -1,28 +1,21 @@
-# -*- coding:utf-8 -*-
 class Solution:
-    # s, pattern都是字符串
-    def match(self, s, pattern):
-        # write code here
-        # 如果两者都为空，则匹配成功
-        if len(s) == 0 and len(pattern) == 0:
-            return True
-        # 如果模式为空，字符串不为空，则匹配不成功
-        if len(s) > 0 and len(pattern) == 0:
-            return False
+    def match(self, s, p):
+        if not s and not p: return True
 
-        if len(pattern) > 1 and pattern[1] == '*':
-            if s and (pattern[0] == '.' or s[0] == pattern[0]):
-                f1 = self.match(s[1:], pattern)  # 多个
-                f2 = self.match(s[1:], pattern[2:])  # 一个
-                f3 = self.match(s, pattern[2:])  # 零个
-                if f1 or f2 or f3:
-                    return True
+        m, n = len(s) + 1, len(p) + 1
+        dp = [[False] * n for _ in range(m)]
+        # 初始化
+        dp[0][0] = True
+        for j in range(2, n, 2):
+            dp[0][j] = dp[0][j - 2] and p[j - 1] == '*'
+        # 状态转移
+        for i in range(1, m):
+            for j in range(1, n):
+                if p[j - 1] == '*':
+                    if dp[i][j - 2]: dp[i][j] = True
+                    elif dp[i - 1][j] and s[i - 1] == p[j - 2]: dp[i][j] = True
+                    elif dp[i - 1][j] and p[j - 2] == '.': dp[i][j] = True
                 else:
-                    return False
-            else:
-                return self.match(s, pattern[2:])
-        elif s and (pattern[0] == '.' or s[0] == pattern[0]):
-            return self.match(s[1:], pattern[1:])
-            # 如果字符串为空，模式不为空，但模式长度等于1，或者模式长度大于1但第二个字符不为’*‘，则匹配不成功
-        else:
-            return False
+                    if dp[i - 1][j - 1] and s[i - 1] == p[j - 1]: dp[i][j] = True
+                    elif dp[i - 1][j - 1] and p[j - 1] == '.': dp[i][j] = True
+        return dp[-1][-1]
